e0c6bd6b52ca9e05b96c045d538d7e2a89134e4e
sshmatrix <cryptosauron@protonmail.com>
2024-12-09 10:15:40 +0530

    Token Balances

--- src/utils/solana.js
+++ src/utils/solana.js
@@ -2,9 +2,9 @@ import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 const endpoint = BASE_URL === "http://localhost:3000" && process.env.NEXT_PUBLIC_SOL_RPC ? process.env.NEXT_PUBLIC_SOL_RPC :  clusterApiUrl("mainnet-beta");
-const connection = new Connection(endpoint); 
+const connection = new Connection(endpoint);
 
-// Token Programs
+// Token Programs [.env]
 export const ANTI_TOKEN_MINT = new PublicKey(process.env.NEXT_PUBLIC_ANTI_TOKEN_MINT);
 export const PRO_TOKEN_MINT = new PublicKey(process.env.NEXT_PUBLIC_PRO_TOKEN_MINT);
 
